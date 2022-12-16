79389ccd8474bc561937634e8cfe9634f19897bc
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2022-12-16 20:36:12 +0100

    changed bg color of body

--- src/js/feed/postTemplate.mjs
+++ src/js/feed/postTemplate.mjs
@@ -4,7 +4,7 @@ export function postContent(post) {
   const singlePost = document.createElement("div");
   postContainer.appendChild(singlePost);
 
-  singlePost.innerHTML = `<div class="card p-3 mb-5 bg-white shadow border-0">
+  singlePost.innerHTML = `<div class="card p-3 mb-5 bg-white shadow border-0" id="post-card">
   <a href = "post/" class="text-decoration-none text-black">
   <p class="text-secondary"> id: ${post.id}</p>
 <h3 class="card-title">${post.title}</h3>
@@ -12,23 +12,24 @@ export function postContent(post) {
 <img class="card-img img-responsive" src="${post.media}"  />
 <p class="card-body">${post.body}</p>
 <p class="text-secondary">${post.tags}</p>
-<div id="authorized-features">
 
-</div>
 </a>
 </div>`;
-  const authorizedFeatures = document.querySelector("#authorized-features");
-  var isOwner = checkPostOwner(post);
 
+  var isOwner = checkPostOwner(post);
+  const authorizedButtons = document.createElement("div");
+  const postCard = singlePost.querySelector("#post-card");
   if (isOwner === true) {
     console.log("is the owner");
+    postCard.appendChild(authorizedButtons);
+    authorizedButtons.classList = "";
+    authorizedButtons.innerHTML = `<button class="btn btn-info text-white">Edit Post</button>
+    <button class="btn btn-danger">Delete Post</button>`;
+
     /* authorizedFeatures.appendChild(authorizedButtons)`<button class="btn btn-info text-white">Edit Post</button>
 <button class="btn btn-danger">Delete Post</button>`; */
-    authorizedFeatures.innerHTML = `<button class="btn btn-info text-white">Edit Post</button>
-<button class="btn btn-danger">Delete Post</button>`;
   } else {
     console.log("Is not the owner");
-    authorizedFeatures.innerHTML = "";
   }
   /* function showAuthorizedFeatures() {
     if (isOwner) {
